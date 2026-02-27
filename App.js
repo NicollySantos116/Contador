77b57fa36e0230d252bb9429265e4ef78201e190
nicolly <saantos.nick1606@gmail.com>
2026-02-27 11:38:56 -0300

    feat: adiciona botão de reset

--- App.js
+++ App.js
@@ -4,13 +4,9 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default function App() {
   const [contador, setContador] = useState(0);
 
-  const incrementar = () => {
-    setContador(contador + 1);
-  };
-
-  const decrementar = () => {
-    setContador(contador - 1);
-  };
+  const incrementar = () => setContador(contador + 1);
+  const decrementar = () => setContador(contador - 1);
+  const resetar = () => setContador(0);
 
   return (
     <View style={styles.container}>
@@ -19,22 +15,65 @@ export default function App() {
 
       <View style={styles.linhaDeBotoes}>
         <TouchableOpacity style={styles.botaoMais} onPress={incrementar}>
-          <Text style={{ color: '#fff', fontWeight: 'bold' }}>+</Text>
+          <Text style={styles.textoBotao}>+</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.botaoMenos} onPress={decrementar}>
-          <Text style={{ color: '#fff', fontWeight: 'bold' }}>-</Text>
+          <Text style={styles.textoBotao}>-</Text>
         </TouchableOpacity>
       </View>
+
+      <TouchableOpacity style={styles.botaoReset} onPress={resetar}>
+        <Text style={styles.textoBotao}>Reset</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#ff8ebb' },
-  titulo: { fontSize: 20, color: '#fff' },
-  numero: { fontSize: 80, fontWeight: 'bold', color: '#fff' },
-  linhaDeBotoes: { flexDirection: 'row', marginTop: 20 },
-  botaoMais: { backgroundColor: '#c01f8a', marginRight: 10, padding: 10, borderRadius: 5 },
-  botaoMenos: { backgroundColor: '#7b0ce4', padding: 10, borderRadius: 5 }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff8ebb',
+  },
+  titulo: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  numero: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  linhaDeBotoes: {
+    flexDirection: 'row',
+    marginTop: 20
+  },
+  botaoMais: {
+    backgroundColor: '#c01f8a',
+    marginRight: 10,
+    padding: 20,
+    borderRadius: 50,
+  },
+  botaoMenos: {
+    backgroundColor: '#7b0ce4',
+    padding: 20,
+    borderRadius: 50,
+  },
+  botaoReset: {
+    marginTop: 20,
+    backgroundColor: '#f8449e',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+  },
+  textoBotao: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
